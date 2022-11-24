a1ee8de9ef84904674bee294c949b42fe5735cba
oskin1 <ilya.arcadich@gmail.com>
2022-11-24 18:29:37 +0300

    MempoolSync init.

--- ergo-mempool-sync/src/client/node.rs
+++ ergo-mempool-sync/src/client/node.rs
@@ -5,4 +5,5 @@ use ergo_lib::chain::transaction::Transaction;
 pub trait ErgoNetwork {
     async fn fetch_mempool(&self, offset: usize, limit: usize) -> Vec<Transaction>;
     async fn fetch_last_blocks(&self, n: usize) -> Vec<FullBlock>;
+    async fn get_best_height(&self) -> u32;
 }
