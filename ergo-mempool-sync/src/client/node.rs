f95dd31db55f1264de79a889db7a3027e51d3fb3
oskin1 <ilya.arcadich@gmail.com>
2022-11-24 13:49:33 +0300

    MempoolSync impl.

---
+++ ergo-mempool-sync/src/client/node.rs
@@ -0,0 +1,8 @@
+use ergo_chain_sync::client::model::FullBlock;
+use ergo_lib::chain::transaction::Transaction;
+
+#[async_trait::async_trait(?Send)]
+pub trait ErgoNetwork {
+    async fn fetch_mempool(&self, offset: usize, limit: usize) -> Vec<Transaction>;
+    async fn fetch_last_blocks(&self, n: usize) -> Vec<FullBlock>;
+}
