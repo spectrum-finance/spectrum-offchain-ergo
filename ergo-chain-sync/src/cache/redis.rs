2f7070bec872f417b69c7c44a22d89cedfad4779
Tim Ling <791016+kettlebell@users.noreply.github.com>
2022-11-28 00:08:54 +1100

    Fix bugs in `RedisClient`, add tests

--- ergo-chain-sync/src/cache/redis.rs
+++ ergo-chain-sync/src/cache/redis.rs
@@ -23,6 +23,12 @@ impl RedisClient {
         let pool = cfg.create_pool(Some(Runtime::AsyncStd1)).unwrap();
         Self { pool }
     }
+
+    /// Clears out the entire redis DB
+    pub async fn flush_db(&self) {
+        let mut conn = self.pool.get().await.unwrap();
+        let _: () = cmd("FLUSHDB").query_async(&mut conn).await.unwrap();
+    }
 }
 
 #[async_trait(?Send)]
@@ -38,7 +44,7 @@ impl ChainCache for RedisClient {
         // SET {block_id_hex}:p {parent_id_hex}
         let _: () = cmd("SET")
             .arg(&parent_id_key)
-            .arg(parent_id_hex)
+            .arg(&parent_id_hex)
             .query_async(&mut conn)
             .await
             .unwrap();
@@ -72,10 +78,14 @@ impl ChainCache for RedisClient {
                 .unwrap();
         }
 
-        // SET best_block {block_id_hex}
-        let _: () = cmd("SET")
+        // DEL best_block
+        let _: () = cmd("DEL").arg(BEST_BLOCK).query_async(&mut conn).await.unwrap();
+
+        // RPUSH best_block {block_id_hex} {parent_id_hex}
+        let _: () = cmd("RPUSH")
             .arg(BEST_BLOCK)
             .arg(&block_id_hex)
+            .arg(&parent_id_hex)
             .query_async(&mut conn)
             .await
             .unwrap();
@@ -95,12 +105,18 @@ impl ChainCache for RedisClient {
 
     async fn get_best_block(&mut self) -> Option<BlockRecord> {
         let mut conn = self.pool.get().await.unwrap();
-        let block_id_hex: Option<String> = cmd("GET").arg(BEST_BLOCK).query_async(&mut conn).await.unwrap();
-        if let Some(block_id_hex) = block_id_hex {
-            let digest32 = Digest32::try_from(block_id_hex.clone()).unwrap();
+        let block_id_hex: Vec<String> = cmd("LRANGE")
+            .arg(BEST_BLOCK)
+            .arg(0)
+            .arg(0)
+            .query_async(&mut conn)
+            .await
+            .unwrap();
+        if !block_id_hex.is_empty() {
+            let digest32 = Digest32::try_from(block_id_hex[0].clone()).unwrap();
             let id = BlockId(digest32);
             let height: u32 = cmd("GET")
-                .arg(&concat_string!(block_id_hex, ":h"))
+                .arg(&concat_string!(block_id_hex[0], ":h"))
                 .query_async(&mut conn)
                 .await
                 .unwrap();
@@ -115,14 +131,32 @@ impl ChainCache for RedisClient {
         if let Some(BlockRecord { id, height }) = self.get_best_block().await {
             let _: () = cmd("DEL").arg(BEST_BLOCK).query_async(&mut conn).await.unwrap();
             let block_id_hex = String::from(id.0.clone());
+            let block_id_digest32 = Digest32::try_from(block_id_hex.clone()).unwrap();
+            let block_id = BlockId(block_id_digest32);
 
             let parent_id_hex: String = cmd("GET")
                 .arg(concat_string!(block_id_hex, ":p"))
                 .query_async(&mut conn)
                 .await
                 .unwrap();
-            let parent_digest32 = Digest32::try_from(parent_id_hex).unwrap();
+            let parent_digest32 = Digest32::try_from(parent_id_hex.clone()).unwrap();
             let parent_id = BlockId(parent_digest32);
+
+            // The new best block will now be the parent of the old best block.
+            if let Ok(new_parent_id_hex) = cmd("GET")
+                .arg(concat_string!(parent_id_hex, ":p"))
+                .query_async::<_, String>(&mut conn)
+                .await
+            {
+                // RPUSH best_block {parent_id_hex} {new_parent_id_hex}
+                let _: () = cmd("RPUSH")
+                    .arg(BEST_BLOCK)
+                    .arg(&parent_id_hex)
+                    .arg(&new_parent_id_hex)
+                    .query_async(&mut conn)
+                    .await
+                    .unwrap();
+            }
 
             let transaction_ids: Vec<String> = cmd("SMEMBERS")
                 .arg(concat_string!(block_id_hex, ":t"))
@@ -132,13 +166,20 @@ impl ChainCache for RedisClient {
 
             let mut transactions = Vec::with_capacity(transaction_ids.len());
             for tx_id_hex in transaction_ids {
-                let tx_json: String = cmd("GET").arg(&tx_id_hex).query_async(&mut conn).await.unwrap();
+                let tx_json: String = cmd("GETDEL")
+                    .arg(&tx_id_hex)
+                    .query_async(&mut conn)
+                    .await
+                    .unwrap();
                 let tx: Transaction = serde_json::from_str(&tx_json).unwrap();
                 transactions.push(tx);
             }
 
+            // DEL best_block
+            let _: () = cmd("DEL").arg(BEST_BLOCK).query_async(&mut conn).await.unwrap();
+
             Some(Block {
-                id,
+                id: block_id,
                 parent_id,
                 height,
                 transactions,
@@ -146,5 +187,73 @@ impl ChainCache for RedisClient {
         } else {
             None
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use ergo_lib::chain::transaction::Transaction;
+    use ergo_lib::ergo_chain_types::{BlockId, Digest32};
+    use sigma_test_util::force_any_val;
+
+    use crate::cache::chain_cache::InMemoryCache;
+    use crate::model::Block;
+
+    #[async_std::test]
+    async fn test_redis() {
+        let mut client = RedisClient::new("redis://127.0.0.1/");
+        test_client(client).await;
+    }
+
+    async fn test_client<C: ChainCache>(mut client: C) {
+        let block_ids: Vec<_> = force_any_val::<[Digest32; 30]>()
+            .into_iter()
+            .map(BlockId)
+            .collect();
+        let mut height = 1;
+
+        let first_id = block_ids[0].clone();
+        let mut blocks = vec![];
+
+        for i in 1..30 {
+            let transactions = force_any_val::<[Transaction; 10]>().to_vec();
+            let parent_id = block_ids[i - 1].clone();
+            let id = block_ids[i].clone();
+            let block = Block {
+                id: id.clone(),
+                parent_id,
+                height,
+                transactions,
+            };
+            blocks.push(block.clone());
+            height += 1;
+
+            client.append_block(block).await;
+            assert!(client.exists(id).await);
+        }
+
+        assert!(!client.exists(first_id).await);
+
+        // Now pop off best blocks
+        while let Some(b0) = client.take_best_block().await {
+            let b1 = blocks.pop().unwrap();
+            assert_eq!(b0.id, b1.id);
+            assert_eq!(b0.parent_id, b1.parent_id);
+            assert_eq!(b0.height, b1.height);
+
+            // Check that the collection of transactions coincide.
+            assert_eq!(b0.transactions.len(), b1.transactions.len());
+            for tx0 in b0.transactions {
+                let tx1 = b1.transactions.iter().find(|t| tx0.id() == t.id()).unwrap();
+                assert_eq!(tx0, *tx1);
+            }
+        }
+    }
+
+    #[async_std::test]
+    async fn test_inmemory_cache() {
+        test_client(InMemoryCache::new()).await;
     }
 }
