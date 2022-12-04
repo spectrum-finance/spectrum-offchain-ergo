b3d6c8f91c2101fd2702c71c54cc67b34f372c5b
oskin1 <ilya.arcadich@gmail.com>
2022-12-04 13:52:15 +0300

    Review comments addressed.

--- spectrum-offchain/src/event_sink/types.rs
+++ spectrum-offchain/src/event_sink/types.rs
@@ -2,7 +2,7 @@ use async_trait::async_trait;
 
 #[async_trait(?Send)]
 pub trait EventHandler<TEvent> {
-    /// Tries to handle the gicen event if applicable.
+    /// Tries to handle the given event if applicable.
     /// Returns `Some(TEvent)` back otherwise.
     async fn try_handle(&mut self, ev: TEvent) -> Option<TEvent>;
 }
