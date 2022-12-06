f851463dc053566315bccaffbc42e8e848a3e6d2
oskin1 <ilya.arcadich@gmail.com>
2022-12-06 17:34:52 +0300

    Pool Ops testing.

--- spectrum-offchain-lm/src/data/redeemer.rs
+++ spectrum-offchain-lm/src/data/redeemer.rs
@@ -4,16 +4,19 @@ use spectrum_offchain::domain::TypedAssetAmount;
 
 use crate::data::assets::{BundleKey, Lq, Reward};
 
+#[derive(Eq, PartialEq, Debug, Clone)]
 pub struct RewardOutput {
     pub reward: TypedAssetAmount<Reward>,
     pub redeemer_prop: ErgoTree,
 }
 
+#[derive(Eq, PartialEq, Debug, Clone)]
 pub struct DepositOutput {
     pub bundle_key: TypedAssetAmount<BundleKey>,
     pub redeemer_prop: ErgoTree,
 }
 
+#[derive(Eq, PartialEq, Debug, Clone)]
 pub struct RedeemOutput {
     pub lq: TypedAssetAmount<Lq>,
     pub redeemer_prop: ErgoTree,
