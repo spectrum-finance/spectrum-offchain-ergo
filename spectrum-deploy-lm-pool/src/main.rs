09995f719a014f01ff9aaf87405bd6ef04805eef
Tim Ling <791016+kettlebell@users.noreply.github.com>
2023-11-17 01:16:14 +1100

    Move Explorer struct to lib.rs

--- spectrum-deploy-lm-pool/src/main.rs
+++ spectrum-deploy-lm-pool/src/main.rs
@@ -11,7 +11,7 @@ use ergo_lib::chain::transaction::{Transaction, TxIoVec};
 use ergo_lib::ergotree_interpreter::sigma_protocol::prover::ContextExtension;
 use ergo_lib::ergotree_ir::chain::address::{Address, AddressEncoder, NetworkPrefix};
 use ergo_lib::ergotree_ir::chain::ergo_box::box_value::{BoxValue, BoxValueError};
-use ergo_lib::ergotree_ir::chain::ergo_box::{BoxId, ErgoBox, NonMandatoryRegisterId};
+use ergo_lib::ergotree_ir::chain::ergo_box::{ErgoBox, NonMandatoryRegisterId};
 use ergo_lib::ergotree_ir::chain::token::{Token, TokenAmount, TokenId};
 use ergo_lib::ergotree_ir::serialization::SigmaParsingError;
 use ergo_lib::ergotree_ir::sigma_protocol::sigma_boolean::{ProveDlog, SigmaProp};
@@ -19,8 +19,9 @@ use ergo_lib::wallet::box_selector::{BoxSelector, BoxSelectorError, SimpleBoxSel
 use ergo_lib::wallet::signing::TxSigningError;
 use ergo_lib::wallet::tx_builder::TxBuilderError;
 use isahc::prelude::Configurable;
-use isahc::{AsyncReadResponseExt, HttpClient};
-use serde::{Deserialize, Serialize};
+use isahc::HttpClient;
+use serde::Deserialize;
+use spectrum_deploy_lm_pool::{Explorer, ExplorerError};
 use spectrum_offchain::domain::{TypedAsset, TypedAssetAmount};
 use spectrum_offchain::event_sink::handlers::types::IntoBoxCandidate;
 use spectrum_offchain::network::ErgoNetwork;
@@ -31,66 +32,11 @@ use spectrum_offchain_lm::data::redeemer::DepositOutput;
 use spectrum_offchain_lm::data::PoolId;
 use thiserror::Error;
 
-use ergo_chain_sync::client::types::{with_path, Url};
+use ergo_chain_sync::client::types::Url;
 use spectrum_offchain::transaction::TransactionCandidate;
 use spectrum_offchain_lm::data::pool::{Pool, ProgramConfig};
 use spectrum_offchain_lm::ergo::{NanoErg, MAX_VALUE, MIN_SAFE_BOX_VALUE, MIN_SAFE_FAT_BOX_VALUE};
 use spectrum_offchain_lm::prover::{SeedPhrase, SigmaProver, Wallet};
-
-pub struct Explorer {
-    pub client: HttpClient,
-    pub base_url: Url,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Items<A> {
-    items: Vec<A>,
-}
-
-impl Explorer {
-    pub async fn get_utxos(&self, addr: &Address) -> Result<Vec<ErgoBox>, ExplorerError> {
-        Ok(self
-            .client
-            .get_async(with_path(
-                &self.base_url,
-                &format!(
-                    "/api/v1/boxes/unspent/byAddress/{}",
-                    AddressEncoder::encode_address_as_string(NetworkPrefix::Mainnet, addr),
-                ),
-            ))
-            .await?
-            .json::<Items<ErgoBox>>()
-            .await?
-            .items)
-    }
-
-    pub async fn get_box(&self, box_id: BoxId) -> Result<ErgoBox, ExplorerError> {
-        let mut response = self
-            .client
-            .get_async(with_path(&self.base_url, &format!("/api/v1/boxes/{}", box_id)))
-            .await?;
-        let ergo_box = if response.status().is_success() {
-            response.json::<ErgoBox>().await?
-        } else {
-            return Err(ExplorerError::UnsuccessfulRequest(
-                "expected 200 from /boxes/".into(),
-            ));
-        };
-        Ok(ergo_box)
-    }
-}
-
-#[derive(Error, From, Debug)]
-pub enum ExplorerError {
-    #[error("json decoding: {0}")]
-    Json(serde_json::Error),
-    #[error("isahc: {0}")]
-    Isahc(isahc::Error),
-    #[error("unsuccessful request: {0}")]
-    UnsuccessfulRequest(String),
-    #[error("No Box found")]
-    NoBox,
-}
 
 #[derive(Debug, Error, From)]
 pub enum Error {
